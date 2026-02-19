f713dc5d56bf2b44f8c4e599199291f15888918a
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2026-02-19 17:19:52 +0200

    problem 15

---
+++ Arrays/Valid Palindrome II.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-palindrome-ii/
+
+# Time complexity : O(n)
+# Space complexity : O(n)
+# Two-pointer
+class Solution(object):
+    def validPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                skipL, skipR = s[l+1 : r+1], s[l:r]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+            l += 1
+            r += 1
+        return True
