da0282707fe7a25304f46bab4acc629064a41f5c
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2026-02-25 00:47:33 +0200

    solution of (Merge Sorted Array)

--- Arrays/Merge Sorted Array.py
+++ Arrays/Merge Sorted Array.py
@@ -1,16 +1,22 @@
 # https://leetcode.com/problems/merge-sorted-array/
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
-res = []
-a, b = 0, 1
-for i in range(m):
-    res.append(nums1[i])
-for j in range(n):
-    res.append(nums2[j])
-while a < b:
-    if res[b] < res[a]:
-        res[a], res[b] = res[b], res[a]
 
-print(res)
+
+# two pointers
+# Time: O(m + n)
+# Space: O(1)
+def merge(nums1, m, nums2, n):
+    i, j = m - 1, n - 1
+    last = m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[last] = nums1[i]
+            i -= 1
+        else:
+            nums1[last] = nums2[j]
+            j -= 1
+        last -= 1
+    
+    while j >= 0:
+        nums1[last] = nums2[j]
+        j -= 1
+        last -= 1
